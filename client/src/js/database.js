b171e4045b14fc738a9cffab3e15e4115e30e7fd
michaelclancy90 <michaelclancy@hotmail.co.uk>
2022-09-29 20:54:58 +1000

    readme.md added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -19,6 +19,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
   const request = store.add({ id: 1, value: content });
   const result = await request;
+  console.log(result);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
@@ -26,7 +27,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   return result.value;
 };
